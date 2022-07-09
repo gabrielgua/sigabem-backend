69a47a40559edbb5bba008442329e10ef73b1557
gabrielgua <gabrieljmym13@gmail.com>
2022-07-09 16:53:47 -0300

    criados services e metodos para consumo de api externa

--- src/main/java/com/projetoapi/controller/ConsultaApiController.java
+++ src/main/java/com/projetoapi/controller/ConsultaApiController.java
@@ -1,13 +1,27 @@
 package com.projetoapi.controller;
 
+import com.projetoapi.model.ConsultaApi;
+import com.projetoapi.service.ConsultaApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ConsultaApiController {
 
+    @Autowired
+    private ConsultaApiService service;
+
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/consultar/{cep}")
+    public ResponseEntity<?> buscarCepPorCodigo(@PathVariable("cep") Long cep) {
+        ConsultaApi consultaApi = service.buscarCep(cep);
+        return ResponseEntity.ok().body(consultaApi);
     }
 }
--- src/main/java/com/projetoapi/service/ConsultaApiService.java
+++ src/main/java/com/projetoapi/service/ConsultaApiService.java
@@ -1,4 +1,20 @@
 package com.projetoapi.service;
 
+import com.projetoapi.model.ConsultaApi;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class ConsultaApiService {
+
+    public ConsultaApi buscarCep(Long cep) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        restTemplate = restTemplateBuilder.build();
+
+        ConsultaApi consultaApi = restTemplate.getForObject("https://viacep.com.br/ws/"+cep+"/json", ConsultaApi.class);
+        return consultaApi;
+    }
 }
